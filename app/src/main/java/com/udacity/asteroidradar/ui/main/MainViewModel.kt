0424e0863afe96032fb1f6681e0c98ccdd7a3d6c
amrudesh1 <amrbal@gmail.com>
2020-03-18 16:31:51 +0530

    Completed MainFragment and Network Calls

--- app/src/main/java/com/udacity/asteroidradar/ui/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/ui/main/MainViewModel.kt
@@ -4,20 +4,27 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.udacity.asteroidradar.data.local.db.DatabaseService
 import com.udacity.asteroidradar.data.model.Asteroid
+import com.udacity.asteroidradar.data.model.PictureOfDay
 import com.udacity.asteroidradar.data.remote.NetworkUtils
 import com.udacity.asteroidradar.data.remote.RetrofitInstance
 import kotlinx.coroutines.*
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val mainViewModelJob = Job()
-    var asteriodData = MutableLiveData<List<Asteroid>>()
+    var asteriodData = MediatorLiveData<List<Asteroid>>()
     private val uiScope = CoroutineScope(Dispatchers.Main + mainViewModelJob)
     val databaseService: DatabaseService = DatabaseService.getInstance(application)
-    lateinit var data: LiveData<List<Asteroid>>
+    val imageOfDay = MutableLiveData<PictureOfDay>()
 
     init {
         initData()
@@ -26,18 +33,43 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private fun initData() {
         uiScope.launch {
             setDataforAsteroid()
+            setImageForTheDay()
+        }
+    }
+
+    private fun setImageForTheDay() {
+        val imagefortheDayMap: MutableMap<String, String> = HashMap()
+        imagefortheDayMap["api_key"] = "JebcLPzxmR30MxO6zWCGbGWiaNeTOIZCSGkkOCZ8"
+        GlobalScope.launch {
+            val imageObject =
+                RetrofitInstance.appAPIWithMoshi.getImageForTheDay(imagefortheDayMap).await()
+            setImagedata(imageObject)
+        }
+    }
+
+    private suspend fun setImagedata(imageObject: PictureOfDay) {
+        withContext(Dispatchers.Main)
+        {
+            imageOfDay.value = imageObject
         }
     }
 
     private suspend fun setDataforAsteroid() {
+        val startDate =
+            SimpleDateFormat("yyyy-MM-dd").format(Date(System.currentTimeMillis()))
+        val endDate =
+            SimpleDateFormat("yyyy-MM-dd").format(Date(System.currentTimeMillis() + 604800000L))
+
+
         val asteroidMap: MutableMap<String, String> =
-                HashMap()
-        asteroidMap["start_date"] = "2020-03-18"
-        asteroidMap["end_date"] = "2020-03-25"
+            HashMap()
+        asteroidMap["start_date"] = startDate
+        asteroidMap["end_date"] = endDate
         asteroidMap["api_key"] = "JebcLPzxmR30MxO6zWCGbGWiaNeTOIZCSGkkOCZ8"
         GlobalScope.launch()
         {
-            val dataObject = JSONObject(RetrofitInstance.appAPIWithScalar.getAsteroids(asteroidMap).await())
+            val dataObject =
+                JSONObject(RetrofitInstance.appAPIWithScalar.getAsteroids(asteroidMap).await())
             Log.i("Data", NetworkUtils.parseAsteroidsJsonResult(dataObject).size.toString())
             setLiveDataValueToDB(NetworkUtils.parseAsteroidsJsonResult(dataObject))
         }
@@ -47,27 +79,20 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         withContext(Dispatchers.IO)
         {
             databaseService.asteroidDao.insertAsteroidData(parseAsteroidsJsonResult)
-            getLiveDataFromDB()
+            setLiveData()
         }
 
     }
 
-    private suspend fun getLiveDataFromDB() {
-        withContext(Dispatchers.IO)
-        {
-            setLiveData(databaseService.asteroidDao.getAllAsteroidData())
 
-        }
-    }
-
-    private suspend fun setLiveData(allAsteroidData: LiveData<List<Asteroid>>) {
+    private suspend fun setLiveData() {
         withContext(Dispatchers.Main)
         {
-            asteriodData.value = getAsteroidsfromDb()!!.value
+            asteriodData.value = getAsteroidsfromDb()
         }
     }
 
-    private suspend fun getAsteroidsfromDb(): LiveData<List<Asteroid>>? {
+    private suspend fun getAsteroidsfromDb(): List<Asteroid> {
         return withContext(Dispatchers.IO)
         {
             val data = databaseService.asteroidDao.getAllAsteroidData()
@@ -77,6 +102,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     fun getAsteriodData(): LiveData<List<Asteroid>> {
         return asteriodData
+    }
+
+    fun getCurrentImageData(): LiveData<PictureOfDay> {
+        return imageOfDay
     }
 
 
